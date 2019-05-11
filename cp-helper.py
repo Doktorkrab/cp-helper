508e25f0c903ba22062cef1735fa54150a5a9b0d
doktorkrab <serega.haritontsev@gmail.com>
2019-05-11 12:25:26 +0300

    Move parse_args to cmd package

--- cp-helper.py
+++ cp-helper.py
@@ -1,25 +1,21 @@
-# ! /usr/bin/env python3
+#! /usr/bin/env python3
 """
 cp-helper - Competitive programming tool.
 
 Usage:
-  cf-helper.py
   cf-helper.py (-v|--version)
   cf-helper.py (-h|--help)
+  cf-helper.py config user (add|modify)
+  cf-helper.py config template (add|delete|default)
 
 Options:
   -v --version  Show version.
   -h --help     Show this screen.
 """
-import typing
-
 from docopt import docopt
 
-
-def parse_args(args: typing.Dict[str, bool]) -> None:
-    pass
-
+import cmd
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version="Developer pre-alpha-0.0.1")
-    parse_args(arguments)
+    cmd.parse_args(arguments)
