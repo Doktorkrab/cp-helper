ecae9290c1b94cb9205b20e46e7f989ebb335f9a
Sergey Kharitontsev- <serega.haritontsev@gmail.com>
2019-05-24 10:32:17 +0300

    add save/load methods for client

--- client/client.py
+++ client/client.py
@@ -1,9 +1,29 @@
-from requests.cookies import RequestsCookieJar
+import pickle
+
+from config import SESSION_PATH
 
 
 class Client(object):
-    def __init__(self, username: str = '', cookies=None):
-        self.username: str = username
+    def __init__(self):
+        self.username: str = ''
         self.ftaa: str = ''
         self.bfaa: str = ''
-        self.cookies: RequestsCookieJar = cookies
+        self.cookies = None
+        self.load()
+
+    def save(self):
+        with open(SESSION_PATH, 'wb') as session_file:
+            pickle.dump(self.username, session_file)
+            pickle.dump(self.ftaa, session_file)
+            pickle.dump(self.bfaa, session_file)
+            pickle.dump(self.cookies, session_file)
+
+    def load(self):
+        try:
+            with open(SESSION_PATH, 'rb') as session_file:
+                self.username = pickle.load(session_file)
+                self.ftaa = pickle.load(session_file)
+                self.bfaa = pickle.load(session_file)
+                self.cookies = pickle.load(session_file)
+        except FileNotFoundError:
+            pass
