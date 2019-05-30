d9873121f452df020a173b411699bd12b82636c0
Sergey Kharitontsev- <serega.haritontsev@gmail.com>
2019-05-30 13:02:24 +0300

    Add a contest watch.

--- client/submission.py
+++ client/submission.py
@@ -1,9 +1,54 @@
+from datetime import datetime
+from platform import system
 from re import findall
+from subprocess import call
+from time import sleep, time
+from typing import List
+
+import bs4
 
 from config.config import CodeTemplate, Config
 from .client import Client
 from .contest import Contest
 from .login import get_csrf_token, check_login
+
+VERDICTS = {"RUNTIME_ERROR": "RE",
+            "WRONG_ANSWER": "WA",
+            "TIME_LIMIT_EXCEEDED": "TLE",
+            "MEMORY_LIMIT_EXCEEDED": "MLE",
+            "IDLENESS_LIMIT_EXCEEDED": "ILE",
+            "OK": "OK",
+            "COMPILATION_ERROR": "CE",
+            "TESTING": "Testing",
+            "WAITING": "Waiting"}
+
+
+class Submisson(object):
+    def __init__(self, tag: bs4.element.Tag):
+        self.id: int = int(tag.find('td', {'class': 'id-cell'}).text)
+        self.author: str = tag.find('td', {'class': 'status-party-cell'}).text.strip().rstrip()
+        parameters = tag.find_all('td', {'class': 'status-small'})
+        self.date: datetime = datetime.strptime(parameters[0].text.strip().rstrip(), '%d.%m.%Y %H:%M')
+        self.problem: str = parameters[1].text.strip().rstrip()
+        self.time: str = tag.find('td', {'class': 'time-consumed-cell'}).text.strip().rstrip()
+        self.memory: str = tag.find('td', {'class': 'time-consumed-cell'}).text.strip().rstrip()
+
+        tmp = tag.find('td', {'class': 'status-cell'})
+        self.status: str = 'WAITING' if tmp is None or tmp.span is None else tmp.span['submissionverdict']
+        self.test: int = -1 if tmp is None or \
+                               tmp.find('span', {'class': 'verdict-format-judged'}) is None else \
+            int(tmp.find('span', {'class': 'verdict-format-judged'}).text)
+
+    def __str__(self):
+        ret = ''
+        ret += f'Submission: #{self.id}\n'
+        ret += f'Author: {self.author}\n'
+        ret += f'Date: {self.date.strftime("%d.%m.%Y %H:%M")}\n'
+        ret += f'Problem: {self.problem}\n'
+        ret += f'Status: {VERDICTS[self.status]}' + ('\n' if self.test == -1 else f' #{self.test}\n')
+        ret += f'Time: {self.time}\n'
+        ret += f'Memory: {self.memory}'
+        return ret
 
 
 def find_error(body: str) -> str:
@@ -52,3 +97,44 @@ def submit(contest: Contest, problem_id: str, source_code: str, template: CodeTe
         print('Submitted successfully!')
     else:
         print(f'[ERROR!]{find_error(resp.text)}')
+    watch_submission(contest)
+
+
+def get_submissions(contest: Contest, n: int = 1, all_submissions: bool = False) -> List[Submisson]:
+    cfg = Config()
+    client = Client()
+    session = client.get_session()
+    url = contest.get_url() + '/my' if not all_submissions else '/status'
+
+    resp = session.get(url)
+
+    if not check_login(resp.text, client.username):
+        cfg.login()
+        resp = session.get(url)
+
+    parsed_html = bs4.BeautifulSoup(resp.text, 'html.parser')
+
+    table: bs4.element.Tag = parsed_html.find_all('table', {'class': "status-frame-datatable"})[0]
+    submissions = []
+    for row in table.find_all('tr')[1:]:
+        submissions.append(Submisson(row))
+        if len(submissions) == n:
+            return submissions
+
+
+def watch_submission(contest: Contest):
+    if system() == 'Windows':  # Make ansi work in Windows
+        call('', shell=True)
+    sub = None
+    flag = False
+    while sub is None or sub.status == 'WAITING' or sub.status == 'TESTING':
+        start = time()
+        sub = get_submissions(contest)[0]
+        if flag:
+            for i in range(7):
+                print(u'\u001b[A\u001b[2K', end='')
+        flag = True
+        print(sub)
+        diff = time() - start
+        if diff < 1:
+            sleep(1 - diff)
