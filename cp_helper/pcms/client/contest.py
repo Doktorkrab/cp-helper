5670a7360f6fdf87d18de5d1177cbc32abae40df
doktorkrab <serega.haritontsev@gmail.com>
2019-06-13 14:38:58 +0300

    get status + get langs

--- cp_helper/pcms/client/contest.py
+++ cp_helper/pcms/client/contest.py
@@ -5,10 +5,11 @@ import bs4
 from cp_helper.pcms.client.core import Client
 from cp_helper.pcms.client.login import check_login
 from cp_helper.pcms.config.core import Config
+from cp_helper.pcms.config.lang import Lang, find_languages
 from cp_helper.utils import color
 
 
-class Contest(object):
+class Contest:
     def __init__(self, id: str = '', url: str = ''):
         self.id = id
         self.url = url  # url to switch to that contest
@@ -16,6 +17,9 @@ class Contest(object):
 
     def __str__(self):
         return f'{self.id}(url: {self.url}, problems: {len(self.problems)})'
+
+    def __eq__(self, other):
+        return self.id == other.id
 
 
 def get_contests_list(name: str) -> List[Contest]:
@@ -43,3 +47,47 @@ def get_contests_list(name: str) -> List[Contest]:
         print(color(f"Can't find any contests!", fg='red', bright_fg=True))
         return []
     return [Contest(par.text, par.a['href'] if par.a else '') for par in contest_list.find_all('p')]
+
+
+def get_contest_status(name: str) -> str:
+    cl = Client(name)
+    session = cl.session
+    cfg = Config(name)
+    resp = session.get(cfg.url + 'party/information.xhtml')
+    if resp.status_code != 200 and resp.status_code != 302:
+        print(color(f'Network Error. Status code:{resp.status_code}', fg='red', bright_fg=True))
+        return 'UNKNOWN, 0:00 of 0:00'
+
+    if not check_login(resp.text):
+        print(color(f'Not logged. Relogging as {cfg.username}', fg='cyan', bright_fg=True))
+        cfg.login()
+        resp = session.get(cfg.url + 'party/information.xhtml')
+
+    soup = bs4.BeautifulSoup(resp.text, 'html.parser')
+    clock = soup.find('span', id='running-clock')
+    if clock is None:
+        return 'UNKNOWN, 0:00 of 0:00'
+    return clock.text.splitlines()[0]
+
+
+def get_contest_langs(name: str) -> List[Lang]:
+    cl = Client(name)
+    session = cl.session
+    cfg = Config(name)
+    resp = session.get(cfg.url + 'party/information.xhtml')
+    if resp.status_code != 200 and resp.status_code != 302:
+        print(color(f'Network Error. Status code:{resp.status_code}', fg='red', bright_fg=True))
+        return []
+
+    if not check_login(resp.text):
+        print(color(f'Not logged. Relogging as {cfg.username}', fg='cyan', bright_fg=True))
+        cfg.login()
+
+    status = get_contest_status(name)
+
+    if status.split(',')[0] != 'RUNNING':
+        return []
+
+    resp = session.get(cfg.url + 'party/submit.xhtml')
+
+    return find_languages(resp.text)
